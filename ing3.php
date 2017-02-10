a5ca44ae399a753ba166cf44accb58c844ea34f4
powhound4 <george.hatch@q.com>
2017-02-10 13:45:30 -0700

    Create ing3.php

---
+++ ing3.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	
+	<!-- Add appropriate required meta tags needed for the responsive webpage using bootstrap -->
+	
+	<!-- Add required bootstrap CDN links for style sheet and required scripts -->
+		
+		<meta charset="utf-8">
+		<meta George Hatch>
+    <meta Jacob Colleran>
+		<meta CT310>
+		<meta Ingredient3>
+    <meta IFY>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">	
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="project1.css">
+		<title>IFY-Ingredient 3</title>
+
+	</head>
+
+
+	<body>
+
+	<!-- Use the jumbotron for creating the header for the webpage -->
+
+		<div class="jumbotron">
+		
+        <br></br> <br></br> <br></br> <br></br><br></br><br></br>
+        
+		</div>
+	
+	<!-- Create the collapsing navigation bar -->
+		
+		<?php include('inc.navbar.php');?>
+		
+	<!-- first col-->
+		
+		<div class="container-fluid">
+		<br></br>
+			<div class="row">
+                <div class="col-xs-3">
+                
+                  <!--col 1-->
+
+
+                </div>
+
+	<!-- ingredient -->
+	<div class="col-xs-6">
+	<h3><center><strong>Ingredient 3</strong></center></h3>
+	  <center><img src="ing3.jpg" class="img-square" alt="ing3" width=70% height=70%></center>
+                <br></br>
+                </div>
+   
+	<!-- thrid col -->				
+			<div class="col-xs-3">
+			<!--col 1-->
+                
+                </div>
+
+			</div>
+		</div>
+		
+        <?php include('inc.footer.php');?>
+	</body>
+</html>
