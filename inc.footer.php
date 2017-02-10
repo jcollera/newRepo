4c686c015e4b340a264bf30fb9b5811943616769
powhound4 <george.hatch@q.com>
2017-02-10 13:44:13 -0700

    Create inc.footer.php

---
+++ inc.footer.php
@@ -0,0 +1,2 @@
+<div class="footer">
+        <p><center>&copy; Copyright 2017 George Hatch & Jacob Colleran This site is part of a CSU <a href="https://www.cs.colostate.edu/~ct310/yr2017sp/index.php">CT 310</a> Course Project.</center></p></div>
