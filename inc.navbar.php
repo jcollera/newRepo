6446c1e41b169d917b1939030632d2cb036ad44f
powhound4 <george.hatch@q.com>
2017-02-10 13:43:45 -0700

    Create inc.navbar.php

---
+++ inc.navbar.php
@@ -0,0 +1,27 @@
+
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>       
+                <span class="icon-bar"></span>      
+                <span class="icon-bar"></span>                      
+            </button>
+                <a class="navbar-brand" href="http://www.cs.colostate.edu/~powhound/project1/frontpage.php">IFY</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li><a href="http://www.cs.colostate.edu/~powhound/project1/frontpage.php" >Home</a></li>
+                <li><a href="./aboutus.php" >About Us</a></li>
+                <li class="dropdown">          
+                    <a href="#TODO" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Ingredients<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="./ing1.php">Ingredient 1</a></li>
+                            <li><a href="./ing2.php">Ingredient 2</a></li>
+                            <li><a href="./ing3.php">Ingredient 3</a></li>
+                        </ul>
+                </li>
+            </ul>
+        </div>
+        </div>
+   </nav>
