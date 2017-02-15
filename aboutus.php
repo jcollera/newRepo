01f3e4d09dbdefac9c1e1ca5080ffabcb56d1327
jcollera <jakecolleran@hotmail.com>
2017-02-15 10:04:35 -0700

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -7,12 +7,11 @@
 	<!-- Add required bootstrap CDN links for style sheet and required scripts -->
 		
 		<meta charset="utf-8">
-		<meta George Hatch>
-        <meta Jacob Colleran>
-		<meta CT310>
-		<meta about us>
+		<meta name="author" content="George Hatch, Jake Colleran" />
+		<meta name="keywords" content= "CT310, about us" />
+
         <meta IFY>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">	
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
